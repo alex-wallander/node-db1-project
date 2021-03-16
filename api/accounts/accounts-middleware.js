83ac8b915301eb8c79cc708ac177fb3c2281c156
alex-wallander <alexwallander@gmail.com>
2021-03-15 20:42:43 -0500

    tasks complete

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -9,7 +9,6 @@ exports.checkAccountPayload = (req, res, next) => {
   } else if (req.body.name.trim().length < 3 || req.body.name.trim().length > 100) {
     res.status(400).json({ message: 'name of account must be between 3 and 100' })
   } else if (typeof req.body.budget !== 'number') {
-    console.log(req.body.budget)
     res.status(400).json({ message: 'budget of account must be a number' })
   } else if (req.body.budget < 0 || req.body.budget > 1000000) {
     res.status(400).json({ message:'budget of account is too large or too small'})
