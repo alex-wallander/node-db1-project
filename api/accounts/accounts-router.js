9b8666a7eaae7c0f79372b3e424dc54b8479469e
alex-wallander <alexwallander@gmail.com>
2021-03-15 20:03:46 -0500

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,23 +1,52 @@
 const router = require('express').Router()
 
+const { checkAccountId, checkAccountPayload, checkAccountNameUnique } = require('./accounts-middleware.js');
+
+const Account = require('./accounts-model.js');
+
 router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Account.getAll()
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  res.json(req.account)
+  next();
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
   // DO YOUR MAGIC
+  Account.create(req.body)
+  .then(acc => {
+    res.status(201).json(acc)
+    acc.trim();
+  })
+  .catch(next);
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', checkAccountId, checkAccountPayload, (req, res, next) => {
   // DO YOUR MAGIC
+  Account.updateById(req.params.id, req.body)
+  .then((accs) => {
+    res.status(200).json(accs)
+    accs.trim();
+  })
+  .catch(next);
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  Account.deleteById(req.params.id)
+  .then(() => {
+    res.status(200).json({ message:'account has been deleted' })
+  })
+  .catch(next);
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
@@ -26,6 +55,7 @@ router.use((err, req, res, next) => { // eslint-disable-line
     message: 'something went wrong inside the accounts router',
     errMessage: err.message,
   })
+  next(err);
 })
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const server = express();
 
+const accountRouter = require('./accounts/accounts-router.js');
+
 server.use(express.json());
+
+server.use('/api/accounts', accountRouter);
 
 module.exports = server;
